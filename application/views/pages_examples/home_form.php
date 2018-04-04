e940cce115ea7f7a9db8df5c06edc06876f07b10
wellison Rodrigues <wellison.rodrigues59@gmail.com>
2018-04-04 00:39:25 -0300

    primeiro commit

--- application/views/pages_examples/home_form.php
+++ application/views/pages_examples/home_form.php
@@ -19,18 +19,18 @@
                     <div class="col-md-12">
                         <div class="card">
                             <div class="card-body" style="padding:0">
-                            <div class="row">
-                                <div class="col-md-3">
-                                    <img src="<?php echo base_url() ?>imgs\elemento1.png">
-                                </div>
-                                <div class="col-md-5" style="margin-left:3%; margin-top:2%; margin-bottom:2px">                
-                                      
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img src="<?php echo base_url() ?>imgs\Elemento1.png">
+                                    </div>
+                                    <div class="col-md-5" style="margin-left:3%; margin-top:2%; margin-bottom:2px">
+
                                         <strong style=""> 326 ususarios cadastrados</strong><br>
                                         <br> 326 ususarios cadastrados<br>
                                         326 ususarios cadastrados<br>
                                         326 ususarios cadastrados<br>
                                     </div>
-                            
+
                                 </div>
                             </div>
                         </div>
@@ -41,19 +41,19 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="card">
-                        <div class="card-body" style="padding:0">
-                            <div class="row">
-                                <div class="col-md-3">
-                                    <img src="<?php echo base_url() ?>imgs\elemento2.png">
-                                </div>
-                                    <div class="col-md-5" style="margin-left:3%; margin-top:2%; margin-bottom:2px">                
-                                      
+                            <div class="card-body" style="padding:0">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img src="<?php echo base_url() ?>imgs\Elemento2.png">
+                                    </div>
+                                    <div class="col-md-5" style="margin-left:3%; margin-top:2%; margin-bottom:2px">
+
                                         <strong style=""> 326 ususarios cadastrados</strong><br>
                                         <br> 326 ususarios cadastrados<br>
                                         326 ususarios cadastrados<br>
                                         326 ususarios cadastrados<br>
                                     </div>
-                            
+
                                 </div>
                             </div>
                         </div>
@@ -64,19 +64,19 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="card">
-                        <div class="card-body" style="padding:0">
-                            <div class="row">
-                                <div class="col-md-3">
-                                    <img src="<?php echo base_url() ?>imgs\elemento3.png">
-                                </div>
-                                <div class="col-md-5" style="margin-left:3%; margin-top:2%; margin-bottom:2px">                
-                                      
+                            <div class="card-body" style="padding:0">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                        <img src="<?php echo base_url() ?>imgs\Elemento3.png">
+                                    </div>
+                                    <div class="col-md-5" style="margin-left:3%; margin-top:2%; margin-bottom:2px">
+
                                         <strong style=""> 326 ususarios cadastrados</strong><br>
                                         <br> 326 ususarios cadastrados<br>
                                         326 ususarios cadastrados<br>
                                         326 ususarios cadastrados<br>
                                     </div>
-                            
+
                                 </div>
                             </div>
                         </div>
@@ -89,19 +89,25 @@
                     <div class="card-body">
                         Ultimos 07 dias
                         <hr style="margin-top:20px">
-                        <img src="<?php echo base_url() ?>imgs\user.png" width="25px" height="25px" class="img-responsive" > 326 ususarios cadastrados
+                        <img src="<?php echo base_url() ?>imgs\user.png" width="25px" height="25px"
+                             class="img-responsive"> 326 ususarios cadastrados
                         <hr style="margin-top:20px">
-                        <img src="<?php echo base_url() ?>imgs\user.png" width="25px" height="25px" class="img-responsive" > 326 ususarios cadastrados
+                        <img src="<?php echo base_url() ?>imgs\user.png" width="25px" height="25px"
+                             class="img-responsive"> 326 ususarios cadastrados
                         <hr style="margin-top:20px">
-                        <img src="<?php echo base_url() ?>imgs\user.png" width="25px" height="25px" class="img-responsive" > 326 ususarios cadastrados
+                        <img src="<?php echo base_url() ?>imgs\user.png" width="25px" height="25px"
+                             class="img-responsive"> 326 ususarios cadastrados
                         <hr style="margin-top:20px">
-                        <img src="<?php echo base_url() ?>imgs\user.png" width="25px" height="25px" class="img-responsive" > 326 ususarios cadastrados
+                        <img src="<?php echo base_url() ?>imgs\user.png" width="25px" height="25px"
+                             class="img-responsive"> 326 ususarios cadastrados
                         <hr style="margin-top:20px">
-                        <img src="<?php echo base_url() ?>imgs\user.png" width="25px" height="25px" class="img-responsive" > 326 ususarios cadastrados
+                        <img src="<?php echo base_url() ?>imgs\user.png" width="25px" height="25px"
+                             class="img-responsive"> 326 ususarios cadastrados
                         <hr style="margin-top:20px;">
-                        <img src="<?php echo base_url() ?>imgs\user.png" width="25px" height="25px" class="img-responsive" > 326 ususarios cadastrados
+                        <img src="<?php echo base_url() ?>imgs\user.png" width="25px" height="25px"
+                             class="img-responsive"> 326 ususarios cadastrados
                         <hr style="margin-top:20px;margin-bottom:35px">
-            
+
                         <div class="text-center">
                             <button class="btn btn-default btn-sm" type="submit"
                                     style="border-radius: 5px; "> VER GRÁFICOS
