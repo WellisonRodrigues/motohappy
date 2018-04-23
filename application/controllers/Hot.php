97299158e6f79a6df3ed20bc6eec456bc937b3e8
wellison Rodrigues <wellison.rodrigues59@gmail.com>
2018-04-23 16:18:23 -0300

    new update

--- application/controllers/Hot.php
+++ application/controllers/Hot.php
@@ -23,7 +23,7 @@ class Hot extends CI_Controller
     public function index()
     {
         $this->load->library('Restfull');
-        $endpoint = 'api/v1/admin/products';
+        $endpoint = 'api/v1/admin/hots';
         $metodo = 'GET';
         $params = '';
 
@@ -39,34 +39,97 @@ class Hot extends CI_Controller
 
     public function new_user($id = null)
     {
-        $this->load->library('Restfull');
+        if ($this->input->post('name') and $id == null) {
 
-        if ($id == null and $this->input->post('name')) {
-            $endpoint = 'admin/auth/partners';
+            $this->load->library('Restfull');
+            $endpoint = 'api/v1/admin/hot';
             $metodo = 'POST';
             $params = array(
-                'name' => $this->input->post('name'),
-                'city' => $this->input->post('city'),
-                'address' => $this->input->post('address'),
-                'state' => $this->input->post('state'),
-                'phone' => $this->input->post('phone'),
-                'password' => $this->input->post('phone'),
-                'password_confirmation' => $this->input->post('phone'),
+                'description' => $this->input->post('description'),
+                'value' => $this->input->post('value'),
+                'image' => $this->input->post('image'),
+//                'phone' => $this->input->post('phone'),
+//                'password' => $this->input->post('password'),
+//                'password_confirmation' => $this->input->post('phone'),
 //                'phone'=> $this->input->post('phone'),
 
             );
 
             $response = $this->restfull->cUrl($params, $endpoint, $metodo);
+            $data['message'] = $response['response'];
+            $data['response'] = $response['response'];
+        }
+
+        if ($id != null and $this->input->post('name') == '') {
+            $this->load->library('Restfull');
+            $endpoint = 'api/v1/admin/hot/' . $id;
+            $metodo = 'GET';
+            $params = '';
+            $response = $this->restfull->cUrl($params, $endpoint, $metodo);
+            $data['response'] = $response['response'];
+//            $data['id'] = $id;
+
+        }
+        if ($this->input->post('name') and $id != null) {
+
+            $this->load->library('Restfull');
+            $endpoint = 'api/v1/admin/hot/' . $id;
+            $metodo = 'PATCH';
+            if ($this->input->post('image')) {
+                $params = array(
+                    'description' => $this->input->post('description'),
+                    'value' => $this->input->post('value'),
+                    'image' => $this->input->post('image'),
+//                'phone' => $this->input->post('phone'),
+//                'password' => $this->input->post('password'),
+//                'password_confirmation' => $this->input->post('phone'),
+//                'phone'=> $this->input->post('phone'),
+
+                );
+            } else {
+                $params = array(
+                    'description' => $this->input->post('description'),
+                    'value' => $this->input->post('value'),
+//                    'nickname' => $this->input->post('nickname'),
+//                    'establishments_ids' => [$this->input->post('establishments_ids')],
+//                    'image' => $this->input->post('image'),
+//                'phone' => $this->input->post('phone'),
+//                'password' => $this->input->post('password'),
+//                'password_confirmation' => $this->input->post('phone'),
+//                'phone'=> $this->input->post('phone'),
+
+                );
+            }
+            $response = $this->restfull->cUrl($params, $endpoint, $metodo);
 //            print_r($response);
 //            die;
+            $data['message'] = $response['response'];
             $data['response'] = $response['response'];
-
+//            print_r($this->input->post());
+//            print_r($response);
+//            die;
 
         }
 
+//        $params2 = '';
+//        $endpoint2 = 'api/v1/admin/categories';
+//        $metodo2 = 'GET';
+//        $response2 = $this->restfull->cUrl($params2, $endpoint2, $metodo2);
+//        $data['categories'] = $response2['response'];
         $data['menu'] = true;  // Menu true significa que a pagina tera o menu principal, false deixa a pagina sem menu(menu = header + navbar)
-        $data['view'] = 'pages_examples/partners_form';
+        $data['view'] = 'pages_examples/hot_form';
         $this->load->view('structure/container', $data);
     }
 
+    public
+    function delete($id)
+    {
+        if ($id != null or $id != '') {
+            $endpoint = 'api/v1/admin/hot/' . $id;
+            $metodo = 'DELETE';
+            $params = '';
+            $response = $this->restfull->cUrl($params, $endpoint, $metodo);
+            redirect('Hot');
+        }
+    }
 }
--- application/views/pages_examples/hot_table.php
+++ application/views/pages_examples/hot_table.php
@@ -7,40 +7,46 @@
  */
 
 //print_r($this->session->userdata("user"));
-echo '<pre>';
-print_r($response);
+//echo '<pre>';
+//print_r($response);
 ?>
 
 <div class="container">
     <h2>Hot</h2>
     <div class="row">
         <div class="col-md-12" align="right">
-            <a href="<?php echo base_url() ?>Users/new_user">
+            <a href="<?php echo base_url() ?>Hot/new_user">
                 <button type="button" class="btn btn-default btn-sm"><i class="fas fa-plus"></i></button>
             </a>
         </div>
     </div>
     <div class="row">
-        <?php foreach ($response as $row) { ?>
+        <?php foreach ($response['hots'] as $row) { ?>
             <div class="col-md-4" style="margin-top: 20px">
                 <div class="card">
                     <div class="card-body">
+                        <?php if (isset($row['image']['url'])) { ?>
+                            <!--                            <div class="col-md-12">-->
+                            <img src="<?php echo @$row['image']['url']; ?>"
+                            <!--                            </div>-->
+                        <?php } ?>
                         <div class="row">
-                            <div class="col-md-2">
-                                <img src="<?php echo base_url() ?>imgs/Elemento6.png" class="rounded-circle">
-                            </div>
+
                             <div class="col-md-7">
-                                <b class="card-title"><?php echo $row['name'] ?></b>
+                                <b class="card-title"><?php echo $row['description'] ?> R$<?php echo $row['value'] ?>
+                                    /litro</b>
                                 <p class="card-text">
-                                    <?php echo $row['email'] ?>
+
                                 </p>
                             </div>
                             <div class="col-md-1">
-                                <a href="<?php echo base_url() ?>Partners/new_user"><i class="fas fa-pencil-alt"
-                                                                                    style="color: gray"></i></a>
+                                <a href="<?php echo base_url() ?>Hot/new_user/<?php echo $row['id'] ?>"><i
+                                            class="fas fa-pencil-alt"
+                                            style="color: gray"></i></a>
                             </div>
                             <div class="col-md-1">
-                                <i class="fas fa-times" style="color: gray"></i>
+                                <a href="<?php echo base_url() ?>Hot/delete/<?php echo $row['id'] ?>"><i
+                                            class="fas fa-times" style="color: gray"></i></a>
                             </div>
                         </div>
                         <!--
